0a1e8a84f208cf8c6305904966aa7179123a34d9
Melchor Dominguez <mdmngz411@gmail.com>
2021-12-15 09:52:46 -0500

    - extracting title

--- main.py
+++ main.py
@@ -8,3 +8,15 @@ html_bytes = page.read()
 html = html_bytes.decode('utf-8')
 
 print(html)
+
+title_index = html.find('<title>')
+title_index
+
+start_index = title_index + len('<title>')
+start_index
+
+end_index = html.find('</title>')
+end_index
+
+title = html[start_index:end_index]
+title
